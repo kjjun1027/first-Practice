32158a920f40bd7853a8c2ea8f9d9d5ccaed1693
kjjun1027 <j77570265@gmail.com>
2024-03-12 23:43:45 +0900

    gogo

---
+++ Assets/Practice02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+
+public class Player : MonoBehaviour
+{
+    public string playerName = "Noname";
+    private int currentHP = 100;
+    public void TakeDamage(int damage)
+    {
+        currentHP = damage;
+        Debug.Log(currentHP);
+    }
+}
+public class Practice02 : MonoBehaviour
+{
+    private void Awake()
+    {
+        Player player;
+
+        player = GameObject.Find("PlayerObject").GetComponent<Player>();
+
+        player.playerName = "고박사"
+;
+        player.TakeDamage(10);
+    }
+}
